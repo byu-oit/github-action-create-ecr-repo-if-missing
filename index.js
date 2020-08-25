b926b03a039281d920487202219d1c4f225eb3ca
GaryGSC <GaryGSC@users.noreply.github.com>
2020-08-25 14:57:44 -0600

    Allow adjusting days to retain untagged images and number of tagged images to retain
    Refs: https://byu-oit.slack.com/archives/C015UJ4QBFU/p1597962324006900

--- index.js
+++ index.js
@@ -4,6 +4,17 @@ const AWS = require('aws-sdk')
 async function run () {
   try {
     const repositoryName = getInput('DOCKER_REPO_NAME', { required: true })
+    const daysBeforeExpiringUntaggedImages = getInput('NUM_DAYS_BEFORE_EXPIRING_UNTAGGED_IMAGES', { required: true })
+    const tagPrefix = getInput('TAG_PREFIX')
+    const numImages = getInput('NUM_TAGGED_IMAGES_TO_RETAIN')
+    if (tagPrefix && !numImages) {
+      setFailed('If TAG_PREFIX is provided, NUM_TAGGED_IMAGES_TO_RETAIN is required')
+      return
+    }
+    if (!tagPrefix && numImages) {
+      setFailed('If NUM_TAGGED_IMAGES_TO_RETAIN is provided, TAG_PREFIX is required')
+      return
+    }
 
     const ecr = new AWS.ECR({ apiVersion: '2015-09-21', region: process.env.AWS_REGION })
 
@@ -43,12 +54,25 @@ async function run () {
       rules: [
         {
           rulePriority: 10,
-          description: 'Expire untagged images after 30 days',
+          description: `Expire untagged images after ${daysBeforeExpiringUntaggedImages} days`,
           selection: {
             tagStatus: 'untagged',
             countType: 'sinceImagePushed',
             countUnit: 'days',
-            countNumber: 30
+            countNumber: parseInt(daysBeforeExpiringUntaggedImages, 10)
+          },
+          action: {
+            type: 'expire'
+          }
+        },
+        ...(tagPrefix && numImages) && { // Conditionally add this
+          rulePriority: 20,
+          description: 'Expire old images as new ones are built',
+          selection: {
+            tagStatus: 'tagged',
+            tagPrefixList: [tagPrefix],
+            countType: 'imageCountMoreThan',
+            countNumber: numImages
           },
           action: {
             type: 'expire'
